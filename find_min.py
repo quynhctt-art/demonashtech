4879444915090493ca4cf47ff1da292ae761fe66
quynhctt-art <quynhctt@fpt.com>
2026-07-13 20:02:35 +0700

    Add files via upload

---
+++ find_min.py
@@ -0,0 +1,7 @@
+# Chương trình tìm giá trị nhỏ nhất trong danh sách
+
+numbers = list(map(int, input("Nhập các số, cách nhau bằng dấu cách: ").split()))
+
+min_value = min(numbers)
+
+print("Giá trị nhỏ nhất là:", min_value)
